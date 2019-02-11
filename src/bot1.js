2834d69a8ae946bd4fb239f7e7b917de1281ac40
Daluck21 <47271845+Daluck21@users.noreply.github.com>
2019-02-12 00:57:00 +0700

    Update bot1.js

--- src/bot1.js
+++ src/bot1.js
@@ -14,10 +14,10 @@ Thanks to @Alfathdirk @TCR_TEAM\n\
 | Change it to your authToken / your email & password
 */
 const auth = {
-	authToken: '',
+	authToken: 'EBNnICbTQIkvz1PmgGR5.q7B4GKh9PPYLrAoLk9E6vq.oWQowCEkToUiahlvs4JiN7D2yWtnwoMOBWmtXuHSPhU=',
 	certificate: '',
-	email: '',
-	password: ''
+	email: 'luckyagata21@gmail.com',
+	password: 'ualucky21'
 }
 
 let client =  new LineConnect();
